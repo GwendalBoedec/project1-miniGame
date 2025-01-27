9ce54a0c3bd26403df7f83213e7b3bfff332a82d
Gwendal Boedec <boedec.g@gmail.com>
2025-01-27 09:21:49 +0100

    files initialization

---
+++ src/index.js
@@ -0,0 +1,30 @@
+console.log("heyyyyyyyy that's a start");
+//PLAN
+
+/* CREATE PLAYER
+functionalities
+move right
+jump
+bark 
+//CREATE OBSTACLE
+generation
+constraints
+//CREATE POINTS
+Image: bones
+generation 
+counter increment
+//CREATE Ennemy 1 : CATS
+move quickly to the left. More quickly than naughty dogs ?
+fades when touched by bark
+//CREATE BONUS : DOG BUDDIES
+don't move
+limited generation
+when touched, move at the back of the player ??
+when touched add one live the live counter
+//CREATE Ennemy 2 : Naughty Dogs
+move slowly to the left. Can't be barked at. 
+// CREATE SCREEN GAME OVER
+*/
+
+
+//SET BACKGROUND
